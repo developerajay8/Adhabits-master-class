1d5086f2336450ef5075c55681cb83b9f7352fdd
Ajaysharma <akpancholi8@gmail.com>
2026-05-03 15:03:49 +0530

    video dekht hai bhagwan ka sukriya hoga ager live ho jat hai toh

--- app/components/videotestimonialssection.tsx
+++ app/components/videotestimonialssection.tsx
@@ -8,18 +8,25 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 const videos = [
   {
     id: 1,
+    src: "/videos/IMG_6236 (1).mp4",
+    poster: "/image copy 8.png",
+    caption: "I'm extremely...",
+  },
+  
+  {
+    id: 2,
     src: "/videos/IMG_4275.MP4",
     poster: "/image copy 5.png",
     caption: "Hey this is...",
   },
   {
-    id: 2,
+    id: 3,
     src: "/videos/IMG_5203.MP4",
     poster: "/image copy 6.png",
     caption: "I'm extremely...",
   },
   {
-    id: 3,
+    id: 4,
     src: "/videos/VID-20260131-WA0005.mp4",
     poster: "/image copy 7.png",
     caption: "I'm extremely...",
@@ -127,7 +134,7 @@ export default function VideoTestimonialsSection() {
 
   return (
     <section className="w-full bg-white pt-10 sm:pt-20">
-      <div className="max-w-[1080px] mx-auto px-4 sm:px-6">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-6">
 
         <h2 className="font-barlow font-[600] text-black text-xl sm:text-3xl md:text-4xl text-center mb-8 sm:mb-12">
           Systems Chosen By{" "}
@@ -135,7 +142,7 @@ export default function VideoTestimonialsSection() {
         </h2>
 
         {/* ✅ GRID FIX HERE */}
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-6 md:gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-6 md:gap-8">
           {videos.map((video) => (
             <VideoCard
               key={video.id}
