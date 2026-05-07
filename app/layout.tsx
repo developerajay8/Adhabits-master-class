6691789e5fa4900f33663d6a9145a369eb4cb980
Ajay Sharma <ajay123@gmail.com>
2026-05-07 20:39:50 +0530

    number update commitssssssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2230789274404823');
+            fbq('init', '1609093500184090');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -54,7 +54,7 @@ export default function RootLayout({
         {/* Noscript fallback */}
         <noscript>
           <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2230789274404823&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1609093500184090&ev=PageView&noscript=1"
           />
         </noscript>
       </body>
